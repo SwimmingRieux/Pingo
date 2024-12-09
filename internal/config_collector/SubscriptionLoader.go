36b5a08067b2f9662a8daf724ca06688b9d563fd
ali <ali84mirloho@gmail.com>
2024-12-10 00:43:00 +0330

    little change in SubscriptionLoader.go

--- internal/config_collector/SubscriptionLoader.go
+++ internal/config_collector/SubscriptionLoader.go
@@ -12,25 +12,21 @@ type SubscriptionLoader struct {
 
 func (s SubscriptionLoader) GetSub(url string) (string, error) {
 
-	// Send HTTP GET request
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	// Check for successful response
 	if resp.StatusCode != http.StatusOK {
 		return "", errors.New("Error: HTTP status " + string(rune(resp.StatusCode)))
 	}
 
-	// Read Context Of Subscription Page
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
 
-	//Convert Body To String
 	text := string(body)
 	s.configs = text
 	return text, nil
