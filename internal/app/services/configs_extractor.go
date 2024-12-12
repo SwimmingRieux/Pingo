fe1e72e014d1386d1ec41315da994e6301236755
ali <ali84mirloho@gmail.com>
2024-12-12 14:37:49 +0330

    implement Extract method

--- internal/app/services/configs_extractor.go
+++ internal/app/services/configs_extractor.go
@@ -1,8 +1,30 @@
 package services
 
+import (
+	"encoding/base64"
+	"strings"
+	"time"
+)
+
 type ConfigsExtractor struct{}
 
-func (extractor *ConfigsExtractor) Extract(string) (string, []string) {
-	// todo: decode first, if it's base64
-	// returns a name for group and list of configs in the given string in raw format
+func (extractor *ConfigsExtractor) Extract(configs string) (string, []string) {
+	decoded, err := base64.StdEncoding.DecodeString(configs)
+
+	if err == nil {
+		configs = string(decoded)
+	}
+
+	splitConfigs := strings.Fields(configs)
+
+	validConfigs := []string{}
+	for _, part := range splitConfigs {
+		if strings.HasPrefix(part, "vless") || strings.HasPrefix(part, "vmess") || strings.HasPrefix(part, "trojan") || strings.HasPrefix(part, "shadowsocks") {
+			validConfigs = append(validConfigs, part)
+		}
+	}
+
+	groupName := time.Now().Format("Jan 02 2006 15:04")
+
+	return groupName, validConfigs
 }
