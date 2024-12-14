97a6594f669cea05a0c6d6b5309ef23df8895a0b
ali <ali84mirloho@gmail.com>
2024-12-14 23:22:40 +0330

    change checking encoding configs in configs_extractor.go and prefixes of configs parts

--- internal/app/services/configs_extractor.go
+++ internal/app/services/configs_extractor.go
@@ -9,17 +9,21 @@ import (
 type ConfigsExtractor struct{}
 
 func (extractor *ConfigsExtractor) Extract(configs string) (string, []string) {
-	decoded, err := base64.StdEncoding.DecodeString(configs)
-
-	if err == nil {
-		configs = string(decoded)
+	checkEncoding := CheckEncoding(configs)
+	if checkEncoding {
+		decoded, err := base64.StdEncoding.DecodeString(configs)
+		if err != nil {
+			return "", nil
+		} else {
+			configs = string(decoded)
+		}
 	}
 
 	splitConfigs := strings.Fields(configs)
 
 	validConfigs := []string{}
 	for _, part := range splitConfigs {
-		if strings.HasPrefix(part, "vless") || strings.HasPrefix(part, "vmess") || strings.HasPrefix(part, "trojan") || strings.HasPrefix(part, "shadowsocks") {
+		if strings.HasPrefix(part, "vless://") || strings.HasPrefix(part, "vmess://") || strings.HasPrefix(part, "trojan://") || strings.HasPrefix(part, "ss://") {
 			validConfigs = append(validConfigs, part)
 		}
 	}
@@ -27,4 +31,13 @@ func (extractor *ConfigsExtractor) Extract(configs string) (string, []string) {
 	groupName := time.Now().Format("Jan 02 2006 15:04")
 
 	return groupName, validConfigs
+}
+
+func CheckEncoding(input string) bool {
+	for _, ch := range input {
+		if !(ch == 43 || (ch >= 47 && ch <= 57) || ch == 61 || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+			return false // config wasn't already base64 encoded
+		}
+	}
+	return true // config was already base64 encoded
 }
