776a66bc041152571f255fc3d326770a7c4a515a
sadq <mamsa0205@gmail.com>
2025-02-16 12:07:45 +0330

    edit tests for config_collection_pinger_test.go

--- tests/app_test/services_test/config_collection_pinger_test.go
+++ tests/app_test/services_test/config_collection_pinger_test.go
@@ -1,7 +1,6 @@
 package services_test
 
 import (
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"net"
 	configurations "pingo/configs"
@@ -18,48 +17,6 @@ type MockConfigPinger struct {
 
 func (m *MockConfigPinger) Ping(config entities.Config, domain structs.DomainWithRank, listener net.Listener, configScoresMap *sync.Map) {
 	m.Called(config, domain, listener, configScoresMap)
-}
-
-// todo: AAA
-// todo: parallel and other stuff in that repo
-func TestPingAllConfigs(t *testing.T) {
-
-	for _, testCase := range pingAllConfigTests {
-		t.Run(testCase.name, func(t *testing.T) {
-			t.Parallel()
-			testCase.testFunction(t, testCase.configs, testCase.domains, testCase.listeners)
-		})
-	}
-}
-
-func PingAllConfigsCallEachPairOnceWhenParametersAreValid(t *testing.T, configs []entities.Config, domains []structs.DomainWithRank, listeners []net.Listener) {
-	// Arrange
-	mockConfigPinger := new(MockConfigPinger)
-	mockConfigPinger.On("Ping", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return()
-
-	service := services.NewConfigCollectionPinger(mockConfigPinger, *realConfig)
-
-	configScoresMap := &sync.Map{}
-	var wg sync.WaitGroup
-	// Act
-	service.PingAllConfigs(configs, domains, &wg, listeners, configScoresMap)
-	wg.Wait()
-
-	// Assert
-	for _, config := range configs {
-		for _, domain := range domains {
-			mockConfigPinger.AssertCalled(t, "Ping", config, domain, mock.Anything, configScoresMap)
-		}
-	}
-	assert.Equal(t, len(configs)*len(domains), len(mockConfigPinger.Calls))
-}
-
-type pingAllConfigTest struct {
-	name         string
-	configs      []entities.Config
-	domains      []structs.DomainWithRank
-	listeners    []net.Listener
-	testFunction func(t *testing.T, configs []entities.Config, domains []structs.DomainWithRank, listeners []net.Listener)
 }
 
 var realConfig, _ = configurations.NewConfig()
@@ -86,4 +43,42 @@ var pingAllConfigTests = []pingAllConfigTest{
 		listeners:    make([]net.Listener, 3),
 		testFunction: PingAllConfigsCallEachPairOnceWhenParametersAreValid,
 	},
+}
+
+func TestPingAllConfigs(t *testing.T) {
+	t.Parallel()
+	for _, testCase := range pingAllConfigTests {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.testFunction(t, testCase.configs, testCase.domains, testCase.listeners)
+		})
+	}
+}
+
+func PingAllConfigsCallEachPairOnceWhenParametersAreValid(t *testing.T, configs []entities.Config, domains []structs.DomainWithRank, listeners []net.Listener) {
+	// Arrange
+	mockConfigPinger := new(MockConfigPinger)
+	for _, config := range configs {
+		for _, domain := range domains {
+			mockConfigPinger.On("Ping", config, domain, mock.Anything, mock.Anything).Once()
+		}
+	}
+
+	service := services.NewConfigCollectionPinger(mockConfigPinger, *realConfig)
+
+	configScoresMap := &sync.Map{}
+	var wg sync.WaitGroup
+	// Act
+	service.PingAllConfigs(configs, domains, &wg, listeners, configScoresMap)
+	wg.Wait()
+
+	// Assert
+	mockConfigPinger.AssertExpectations(t)
+}
+
+type pingAllConfigTest struct {
+	name         string
+	configs      []entities.Config
+	domains      []structs.DomainWithRank
+	listeners    []net.Listener
+	testFunction func(t *testing.T, configs []entities.Config, domains []structs.DomainWithRank, listeners []net.Listener)
 }
