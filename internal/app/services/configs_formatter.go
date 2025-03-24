4ca0d286a0810a235493f5e936fa7fe582b9b99b
sadq <mamsa0205@gmail.com>
2025-03-24 13:39:59 +0330

    add other parts of json configs to formatter rather than only outbound

--- internal/app/services/configs_formatter.go
+++ internal/app/services/configs_formatter.go
@@ -1,27 +1,124 @@
 package services
 
-import "github.com/hiddify/ray2sing/ray2sing"
+import (
+	"fmt"
+	"github.com/hiddify/ray2sing/ray2sing"
+	"pingo/configs"
+)
 
-type VmessConfigsFormatter struct{}
+type VmessConfigsFormatter struct {
+	configuration *configs.Configuration
+}
 
 func (formatter *VmessConfigsFormatter) Format(rawConfig string) (string, error) {
-	return ray2sing.Ray2Singbox(rawConfig, true)
+	outbound, err := ray2sing.Ray2Singbox(rawConfig, true)
+	if err != nil {
+		return "", err
+	}
+	dns := formatter.configuration.V2.DNS
+	inbounds := formatter.configuration.V2.Inbounds
+	log := formatter.configuration.V2.Log
+	policy := formatter.configuration.V2.Policy
+	routing := formatter.configuration.V2.Routing
+	stats := formatter.configuration.V2.Stats
+	formattedConfig := fmt.Sprintf(`{
+    "dns": %s,
+    "inbounds": %s,
+    "log": %s,
+    "outbounds": %s,
+    "policy": %s,
+    "routing": %s,
+    "stats": %s
+}`, dns, inbounds, log, outbound, policy, routing, stats)
+
+	return formattedConfig, nil
+
 }
 
-type VlessConfigsFormatter struct{}
+type VlessConfigsFormatter struct {
+	configuration *configs.Configuration
+}
 
 func (formatter *VlessConfigsFormatter) Format(rawConfig string) (string, error) {
-	return ray2sing.Ray2Singbox(rawConfig, true)
+
+	outbound, err := ray2sing.Ray2Singbox(rawConfig, true)
+	if err != nil {
+		return "", err
+	}
+	dns := formatter.configuration.V2.DNS
+	inbounds := formatter.configuration.V2.Inbounds
+	log := formatter.configuration.V2.Log
+	policy := formatter.configuration.V2.Policy
+	routing := formatter.configuration.V2.Routing
+	stats := formatter.configuration.V2.Stats
+	formattedConfig := fmt.Sprintf(`{
+    "dns": %s,
+    "inbounds": %s,
+    "log": %s,
+    "outbounds": %s,
+    "policy": %s,
+    "routing": %s,
+    "stats": %s
+}`, dns, inbounds, log, outbound, policy, routing, stats)
+
+	return formattedConfig, nil
+
 }
 
-type TrojanConfigsFormatter struct{}
+type TrojanConfigsFormatter struct {
+	configuration *configs.Configuration
+}
 
 func (formatter *TrojanConfigsFormatter) Format(rawConfig string) (string, error) {
-	return ray2sing.Ray2Singbox(rawConfig, true)
+	outbound, err := ray2sing.Ray2Singbox(rawConfig, true)
+	if err != nil {
+		return "", err
+	}
+	dns := formatter.configuration.V2.DNS
+	inbounds := formatter.configuration.V2.Inbounds
+	log := formatter.configuration.V2.Log
+	policy := formatter.configuration.V2.Policy
+	routing := formatter.configuration.V2.Routing
+	stats := formatter.configuration.V2.Stats
+	formattedConfig := fmt.Sprintf(`{
+    "dns": %s,
+    "inbounds": %s,
+    "log": %s,
+    "outbounds": %s,
+    "policy": %s,
+    "routing": %s,
+    "stats": %s
+}`, dns, inbounds, log, outbound, policy, routing, stats)
+
+	return formattedConfig, nil
 }
 
-type SsConfigsFormatter struct{}
+type SsConfigsFormatter struct {
+	configuration *configs.Configuration
+}
 
 func (formatter *SsConfigsFormatter) Format(rawConfig string) (string, error) {
-	return ray2sing.Ray2Singbox(rawConfig, true)
+
+	outbound, err := ray2sing.Ray2Singbox(rawConfig, true)
+	if err != nil {
+		return "", err
+	}
+	dns := formatter.configuration.V2.DNS
+	inbounds := formatter.configuration.V2.Inbounds
+	log := formatter.configuration.V2.Log
+	policy := formatter.configuration.V2.Policy
+	routing := formatter.configuration.V2.Routing
+	stats := formatter.configuration.V2.Stats
+	formattedConfig := fmt.Sprintf(`{
+    "dns": %s,
+    "inbounds": %s,
+    "log": %s,
+    "outbounds": %s,
+    "policy": %s,
+    "routing": %s,
+    "stats": %s
+}`, dns, inbounds, log, outbound, policy, routing, stats)
+
+	return formattedConfig, nil
+
 }
